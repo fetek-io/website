52242a20945efd6f0903b7934a51b99f9da26612
dungvv <dungvv@uetunited.com>
2022-08-26 15:34:24 +0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { PageWrapper } from "~components"
+const header = {
+  headerClasses: "site-header site-header--menu-end light-header site-header--sticky",
+  containerFluid:false,
+}
+
+export default function Marketing() {
+  return (
+    <PageWrapper headerConfig={header}>
+	your content goes here...
+    </PageWrapper>
+  )
+}
