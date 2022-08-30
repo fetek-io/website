8cfe3c183ffad8b55d8dd8abfaf06ee55e58d644
hiepn06 <hiepn06@gmail.com>
2022-08-30 11:35:52 +0700

    Update site title

--- src/components/Core/Layout/Layout.js
+++ src/components/Core/Layout/Layout.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import favicon from '~image/mixed/favicon.png';
+import favicon from "~image/mixed/favicon.png";
 import Header from "../Header";
-export default function Layout({
-  children,
-}) {
+export default function Layout({ children }) {
   return (
     <>
-    <Helmet>
-          <title>Fastland Gatsby Landing Template</title>
-          <link rel="icon" type="image/png" href={favicon} />
-    </Helmet>
-      <Header/>
+      <Helmet>
+        <title>FETEK</title>
+        <link rel="icon" type="image/png" href={favicon} />
+      </Helmet>
+      <Header />
       {children}
     </>
-  )
+  );
 }
