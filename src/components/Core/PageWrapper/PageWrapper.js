438970476ee2619fde19c2ead12e2e81c0964830
hiepn06 <hiepn06@gmail.com>
2022-08-30 09:20:58 +0700

    Assets and change homesite

--- src/components/Core/PageWrapper/PageWrapper.js
+++ src/components/Core/PageWrapper/PageWrapper.js
@@ -1,28 +1,36 @@
 import React from "react";
-import GlobalHeaderContext, { headerDefault } from "../../../context/GlobalHeaderContext";
+import GlobalHeaderContext, {
+  headerDefault,
+} from "../../../context/GlobalHeaderContext";
 import Link from "../Link";
-import HeaderButton from "../Header/InnerPageHeader"
+import HeaderButton from "../Header/InnerPageHeader";
 
- const PageWrapper= ({ children, headerConfig = null, innerPage = false, innerPageFooter = false  })=> {  
-    const innerPageDefault = {
-        headerClasses:"light-header site-header--menu-end site-header--button-sep position-relative",
-        containerFluid:false,
-        darkLogo:true,
-        buttonBlock:(<HeaderButton as={Link} btnText="Purchase"/>)
-    }
-    const activeHeader = ( innerPage ? innerPageDefault : headerDefault );  
+const PageWrapper = ({
+  children,
+  headerConfig = null,
+  innerPage = false,
+  innerPageFooter = false,
+}) => {
+  const innerPageDefault = {
+    headerClasses:
+      "light-header site-header--menu-end site-header--button-sep position-relative",
+    containerFluid: false,
+    darkLogo: true,
+    buttonBlock: <HeaderButton as={Link} btnText="Contact us" />,
+  };
+  const activeHeader = innerPage ? innerPageDefault : headerDefault;
   const sitectx = React.useContext(GlobalHeaderContext);
-  
+
   React.useEffect(() => {
-      sitectx.changeHeader({ ...activeHeader, ...headerConfig });
-       // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
+    sitectx.changeHeader({ ...activeHeader, ...headerConfig });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       {/* <Nav /> */}
       {children}
     </>
-  )
-}
+  );
+};
 
 export default PageWrapper;
