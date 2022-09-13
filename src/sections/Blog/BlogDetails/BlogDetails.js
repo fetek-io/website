39dcfae529e01a008ae0702b17302931a3897cea
hiepn06 <hiepn06@gmail.com>
2022-09-13 09:15:25 +0700

    Blog loading

--- src/sections/Blog/BlogDetails/BlogDetails.js
+++ src/sections/Blog/BlogDetails/BlogDetails.js
@@ -25,7 +25,7 @@ const handleGetPreviousBlog = async (blog) => {
 };
 
 export default function BlogDetails({ params }) {
-  const { data } = useQuery([params?.keyword], () =>
+  const { data, isLoading: loading } = useQuery([params?.keyword], () =>
     getBlog({
       keyword: params?.keyword,
     })
@@ -37,117 +37,119 @@ export default function BlogDetails({ params }) {
     <Details backgroundColor="#f9fafc" key={params?.keyword}>
       <Details.Box pb="60px" pbMD="80px" pbLG="130px">
         <Container>
-          <Row className="justify-content-center">
-            {blog && (
-              <Col className="col-lg-7">
-                <Details.Box mb="30px" mbLG="55px">
-                  <Details.Title as="h1">{blog.fields.title}</Details.Title>
-                  <Details.MetaInfo>
-                    <Details.Link
-                      to={`/blog/?authorId=${blog?.fields?.author?.sys?.id}`}
-                      as={Link}
-                    >
-                      By {blog.fields.author.fields.name}
-                    </Details.Link>
-                    <Details.Link to="/" as={Link}>
-                      {new Date(blog?.sys?.createdAt).toDateString()}
-                    </Details.Link>
-                  </Details.MetaInfo>
-                </Details.Box>
-                <Details.Box>
-                  <Details.Image mb="35px" mbLG="55px">
-                    <img
-                      className="w-100"
-                      src={blog?.fields?.banner?.fields?.file?.url}
-                      alt="Blog"
-                      layout="constrained"
-                      placeholder="blurred"
-                    />
-                  </Details.Image>
-                  <div
-                    style={{
-                      letterSpacing: "normal",
-                      lineHeight: "32px",
-                    }}
-                    dangerouslySetInnerHTML={{
-                      __html: documentToHtmlString(blog?.fields?.content),
-                    }}
-                  ></div>
-                  <Details.Tag mt="50px">
-                    <Details.SubTitle mr="25px" mt="10px" as="h4">
-                      Tags:
-                    </Details.SubTitle>
-                    <Details.TagList>
-                      {blog?.fields?.tags.map((item, index) => {
-                        return (
-                          <li>
-                            <Link to={`/blog/?badge=${item}`} key={index}>
-                              {item}
-                            </Link>
-                          </li>
-                        );
-                      })}
-                    </Details.TagList>
-                  </Details.Tag>
-                  <Details.Tag mt="30px">
-                    <Details.SubTitle mr="25px" as="h4">
-                      Share:
-                    </Details.SubTitle>
-                    <Details.SocialList>
-                      <li>
-                        <Link to="/">
-                          <i className="fab fa-instagram" />
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/">
-                          <i className="fab fa-linkedin" />
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/">
-                          <i className="fab fa-facebook" />
-                        </Link>
-                      </li>
-                      <li>
-                        <Link to="/">
-                          <i className="fab fa-twitter" />
-                        </Link>
-                      </li>
-                    </Details.SocialList>
-                  </Details.Tag>
-                  <Details.Pagination mt="30px" mtLG="75px" mb="50px">
-                    <Details.PrevButton>
-                      <span
-                        className="btn-link"
-                        onClick={() => {
-                          handleGetPreviousBlog(blog);
-                        }}
+          {!loading && (
+            <Row className="justify-content-center">
+              {blog && (
+                <Col className="col-lg-7">
+                  <Details.Box mb="30px" mbLG="55px">
+                    <Details.Title as="h1">{blog.fields.title}</Details.Title>
+                    <Details.MetaInfo>
+                      <Details.Link
+                        to={`/blog/?authorId=${blog?.fields?.author?.sys?.id}`}
+                        as={Link}
                       >
-                        <i className="fa fa-arrow-left" />
-                        prev
-                      </span>
-                    </Details.PrevButton>
-                    <Details.NextButton>
-                      <span
-                        className="btn-link"
-                        onClick={() => {
-                          handleGetNextBlog(blog);
-                        }}
-                      >
-                        next
-                        <i className="fa fa-arrow-right" />
-                      </span>
-                    </Details.NextButton>
-                  </Details.Pagination>
-                </Details.Box>
-              </Col>
-            )}
+                        By {blog.fields.author.fields.name}
+                      </Details.Link>
+                      <Details.Link to="/" as={Link}>
+                        {new Date(blog?.sys?.createdAt).toDateString()}
+                      </Details.Link>
+                    </Details.MetaInfo>
+                  </Details.Box>
+                  <Details.Box>
+                    <Details.Image mb="35px" mbLG="55px">
+                      <img
+                        className="w-100"
+                        src={blog?.fields?.banner?.fields?.file?.url}
+                        alt="Blog"
+                        layout="constrained"
+                        placeholder="blurred"
+                      />
+                    </Details.Image>
+                    <div
+                      style={{
+                        letterSpacing: "normal",
+                        lineHeight: "32px",
+                      }}
+                      dangerouslySetInnerHTML={{
+                        __html: documentToHtmlString(blog?.fields?.content),
+                      }}
+                    ></div>
+                    <Details.Tag mt="50px">
+                      <Details.SubTitle mr="25px" mt="10px" as="h4">
+                        Tags:
+                      </Details.SubTitle>
+                      <Details.TagList>
+                        {blog?.fields?.tags.map((item, index) => {
+                          return (
+                            <li>
+                              <Link to={`/blog/?badge=${item}`} key={index}>
+                                {item}
+                              </Link>
+                            </li>
+                          );
+                        })}
+                      </Details.TagList>
+                    </Details.Tag>
+                    <Details.Tag mt="30px">
+                      <Details.SubTitle mr="25px" as="h4">
+                        Share:
+                      </Details.SubTitle>
+                      <Details.SocialList>
+                        <li>
+                          <Link to="/">
+                            <i className="fab fa-instagram" />
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/">
+                            <i className="fab fa-linkedin" />
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/">
+                            <i className="fab fa-facebook" />
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/">
+                            <i className="fab fa-twitter" />
+                          </Link>
+                        </li>
+                      </Details.SocialList>
+                    </Details.Tag>
+                    <Details.Pagination mt="30px" mtLG="75px" mb="50px">
+                      <Details.PrevButton>
+                        <span
+                          className="btn-link"
+                          onClick={() => {
+                            handleGetPreviousBlog(blog);
+                          }}
+                        >
+                          <i className="fa fa-arrow-left" />
+                          prev
+                        </span>
+                      </Details.PrevButton>
+                      <Details.NextButton>
+                        <span
+                          className="btn-link"
+                          onClick={() => {
+                            handleGetNextBlog(blog);
+                          }}
+                        >
+                          next
+                          <i className="fa fa-arrow-right" />
+                        </span>
+                      </Details.NextButton>
+                    </Details.Pagination>
+                  </Details.Box>
+                </Col>
+              )}
 
-            <Col xs="12" className="col-xl-4 offset-xl-1 col-lg-5">
-              <Sidebar />
-            </Col>
-          </Row>
+              <Col xs="12" className="col-xl-4 offset-xl-1 col-lg-5">
+                <Sidebar />
+              </Col>
+            </Row>
+          )}
         </Container>
       </Details.Box>
     </Details>
